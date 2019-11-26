7c7061708acc0e9ec7f2bbe20e8a8c5a178888d2
Sérgio M. Basto <sergio@serjux.com>
2019-11-26 20:06:57 +0000

    Bug fix and raise some errors instead ignore it.

--- svn2github.py
+++ svn2github.py
@@ -28,11 +28,11 @@ def get_last_revision_from_svn(svn_url):
         if m:
             return int(m.group(1))
 
-    return Svn2GithubException("svn info {} output did not specify the current revision".format(svn_url))
+    raise Svn2GithubException("svn info {} output did not specify the current revision".format(svn_url))
 
 
 def run_git_cmd(args, git_dir):
-    return proc.run(["git"] + args, check=True, cwd=git_dir, stderr=DEVNULL, stdin=DEVNULL, stdout=PIPE)
+    return proc.run(["git"] + args, check=True, cwd=git_dir, stdout=PIPE)
 
 
 def is_repo_empty(git_dir):
@@ -50,7 +50,8 @@ def get_svn_info_from_git(git_dir):
         if m:
              return GitSvnInfo(svn_url=m.group(1), svn_revision=int(m.group(2)), svn_uuid=m.group(3))
 
-    return Svn2GithubException("git log -1 HEAD --pretty=%b output did not specify the current revision")
+    print(result)
+    raise Svn2GithubException("git log -1 HEAD --pretty=%b output did not specify the current revision")
 
 
 def git_svn_init(git_svn_info, git_dir):
@@ -136,6 +137,7 @@ def sync_github_mirror(github_repo, cache_dir, new_svn_url=None, new_git_dir=Non
             git_svn_info = GitSvnInfo(svn_url=new_svn_url, svn_revision=0, svn_uuid=None)
         else:
             git_svn_info = get_svn_info_from_git(git_dir)
+            #print(git_svn_info)
             print("svn_url = %s, svn_revison = %s" % (git_svn_info.svn_url, git_svn_info.svn_revision))
 
         print("Checking for SVN updates")
@@ -148,7 +150,7 @@ def sync_github_mirror(github_repo, cache_dir, new_svn_url=None, new_git_dir=Non
             return
 
         print("Fetching from SVN", end="")
-        if not cached or new_svn_url:
+        if (not cached or new_svn_url) and not new_git_dir:
             git_svn_init(git_svn_info, git_dir)
 
         for rev in git_svn_fetch(git_dir):
